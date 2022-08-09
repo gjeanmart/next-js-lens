d441f59f78dfd7ba1d467fa1752f51d415e8357d
gjeanmart <greg@jeanmart.me>
2022-08-09 23:08:57 +0200

    connect metamask

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import ConnectWallet from '../src/components/ConnectWallet/ConnectWallet'
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -13,12 +13,13 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hello World
-        </h1>
+        <h1 className={styles.title}>Hello World</h1>
+        <div>
+          <ConnectWallet />
+        </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/components/ConnectWallet/ConnectWallet.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+
+import { useWeb3React } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+import { InjectedConnector } from "@web3-react/injected-connector";
+
+const ConnectWallet = () => {
+  const injectedConnector = new InjectedConnector({});
+  const { chainId, account, activate, active, library } =
+    useWeb3React<Web3Provider>();
+  const onClick = () => {
+    activate(injectedConnector);
+  };
+
+  useEffect(() => {
+    console.log(chainId, account, active);
+  });
+
+  return (
+    <div>
+      <div>ChainId: {chainId}</div>
+      <div>Account: {account}</div>
+      {active ? (
+        <div>✅ </div>
+      ) : (
+        <button type="button" onClick={onClick}>
+          Connect
+        </button>
+      )}
+    </div>
+  );
+};
+
+
+export default ConnectWallet
